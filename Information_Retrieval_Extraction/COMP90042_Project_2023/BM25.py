replaced TFIDF dictionary with scipy sparse matrix, helped fix memory overflow during eval

Information_Retrieval_Extraction/COMP90042_Project_2023/BM25.py
@@ -1,12 +1,15 @@
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from scipy.sparse import lil_matrix, csr_matrix
 from collections import defaultdict
 from unidecode import unidecode
 import numpy as np
 from tqdm import tqdm
+import math, random
 from memory_profiler import profile
 
+"""
 class IR_System():
     def __init__(self, k = 1.25, b = 0.75, remove_stopwords=False, apply_stemming=False):
         self.k = k
@@ -20,8 +23,12 @@ class IR_System():
             self.stemmer = PorterStemmer()      
 
     def train(self, documents):
-        self.documents = documents
-        self.TFIDF, self.inverted_index, self.doc_tfidf_norms = self.create_inverted_index()
+        # tokenizing documents
+        print("Tokenizing documents...")
+        documents = [self.tokenize(doc) for doc in tqdm(documents, total=len(documents))]
+        self.vocab = list(set([w for doc in documents for w in doc]))
+        self.word2idx = {w:i for i,w in enumerate(self.vocab)}
+        self.TFIDF, self.inverted_index, self.doc_tfidf_norms = self.create_inverted_index(documents)
         
     def tokenize(self, sent):
         # Replace accented letters with regular letters
@@ -36,105 +43,216 @@ class IR_System():
             words = [self.stemmer.stem(w) for w in words]
         return words
 
-    def create_inverted_index(self):
-        N = len(self.documents)
-        TFIDF = defaultdict(float)
-        inverted_index = defaultdict(list)
-
+    def create_inverted_index(self, documents):
         # compute term frequency and document frequencies
-        TF, term_docs = self.compute_TF_weighted()
+        TF, term_docs = self.compute_TF_weighted(documents)
 
         # create inverted index
+        N = len(documents)
+        TFIDF = defaultdict(float)
+        inverted_index = defaultdict(list)
         print(f"Computing TFIDF and creating inverted index...")
         for w, docs in tqdm(term_docs.items(), total=len(term_docs)):
             for d in sorted(list(docs)):
-                tfidf = TF[(w,d)] * math.log10(N/len(docs))
-                inverted_index[w].append(d)
-                TFIDF[(w,d)] = tfidf
+                TFIDF[(self.word2idx[w], d)] = TF[(self.word2idx[w], d)] * math.log10(N/len(docs))
+                inverted_index[self.word2idx[w]].append(d)
 
         # compute document TFIDF vector norms
         print(f"Computing TFIDF vector norms...")
-        doc_tfidf_norms = [0] * N
-        for d, doc in tqdm(enumerate(self.documents), total=len(self.documents)):
-            words = self.tokenize(doc)
+        doc_tfidf_norms = np.zeros(N)
+        for d, words in tqdm(enumerate(documents), total=len(documents)):
             for w in words:
-                doc_tfidf_norms[d] = doc_tfidf_norms[d] +  TFIDF[(w,d)]**2
+                doc_tfidf_norms[d] = doc_tfidf_norms[d] +  TFIDF[(self.word2idx[w], d)]**2
             doc_tfidf_norms[d] = math.sqrt(doc_tfidf_norms[d])
 
         return TFIDF, inverted_index, doc_tfidf_norms  
           
     # weighted TF for BM25
-    def compute_TF_weighted(self):
-        TF = defaultdict(int)
+    def compute_TF_weighted(self, documents):
         term_docs = defaultdict(set)
         doc_length = defaultdict(float)
+        TF = defaultdict(float)
+        TF_weighted = defaultdict(float)
         Dtotal = 0
-        print(f"Computing TFIDF...")
-        for d, doc in tqdm(enumerate(self.documents), total=len(self.documents)):
-            words = self.tokenize(doc)
+        print(f"Computing TF...")
+        for d, words in tqdm(enumerate(documents), total=len(documents)):
             for w in words:
-                TF[(w, d)] += 1
+                TF[(self.word2idx[w], d)] += 1
                 term_docs[w].add(d)
             doc_length[d] = len(words)
             Dtotal += len(words)
-        Davg = Dtotal / len(self.documents)
+        Davg = Dtotal / len(documents)
 
         # compute BM25 weighted term frequencies
-        TF_weighted = defaultdict(float)
-        for (w,d), tf in TF.items():
-            TF_weighted[(w,d)] = (tf * (self.k + 1)) / (tf + self.k * (1 - self.b + self.b * (doc_length[d]/Davg)))
+        for w, docs in term_docs.items():
+            for d in docs:
+                tf = TF[(self.word2idx[w],d)]
+                TF_weighted[(self.word2idx[w],d)] = (tf * (self.k + 1)) / (tf + self.k * (1 - self.b + self.b * (doc_length[d]/Davg)))
         return TF_weighted, term_docs
     
-    @profile 
+    # @profile 
     def retrieve_docs(self, query, topk=1):
         # for retrieval, we will remove stopwords from the query
         query_words = self.tokenize(query.lower())
-        #print(f"query words: {query_words}")
+        # remove out of vocab words
+        query_words = [w for w in query_words if w in self.vocab]
+        #print(f"query words: {query_words}")        
         # get all documents which contain words from query
-        docs = []
-        for w in query_words:
-            docs.extend(d for d in self.inverted_index[w]) 
-        # remove duplicates
-        docs = list(set(docs))
+        docs = list(set(doc for w in query_words for doc in self.inverted_index[self.word2idx[w]]))
         #print(f"docs: {docs}")    
         # score all these documents
-        
-        """
         scores = np.zeros(len(docs))
-        for i in range(len(docs)):
-            d = docs[i]
+        for i, d in enumerate(docs):
             for w in query_words:
-                scores[i] += self.TFIDF[(w,d)]
+                scores[i] += self.TFIDF[(self.word2idx[w],d)]
             scores[i] = scores[i] / self.doc_tfidf_norms[d]        
+    
         #print(f"scores: {scores}")  
-
         sorted_indices = np.argsort(scores)[::-1]
         best_indices = sorted_indices[:topk].tolist()
-        best_scores = scores[best_indices].tolist()
+        topk_scores = scores[best_indices].tolist()
         topk_doc_indices = [docs[idx] for idx in best_indices]
-        del scores  # Explicitly delete the scores variable to free up memory
-        """
-
-        # score all these documents
-        topk_scores = []
-        topk_doc_indices = []
-        for d in docs:
-            score = 0
-            for w in query_words:
-                score += self.TFIDF[(w,d)]
-            score = score / self.doc_tfidf_norms[d]
-            # maintain a list of largest topk scores seen so far and corresponding indices
-            if len(topk_scores) < topk or score > min(topk_scores):
-                if len(topk_scores) == topk:
-                    min_idx = topk_scores.index(min(topk_scores))
-                    topk_scores.pop(min_idx)
-                    topk_doc_indices.pop(min_idx)
-                topk_scores.append(score)
-                topk_doc_indices.append(d)
-
-        # sort top-k scores and their corresponding document indices
-        sorted_indices = np.argsort(topk_scores)[::-1]
-        topk_scores = [topk_scores[idx] for idx in sorted_indices]
-        topk_doc_indices = [topk_doc_indices[idx] for idx in sorted_indices]
+        del scores
+        del docs
         
         return topk_doc_indices, topk_scores
+""" 
+
+# using scipy sparse array instead of dictionary for storing TFIDF
+class IR_System():
+    def __init__(self, k = 1.25, b = 0.75, remove_stopwords=False, apply_stemming=False):
+        self.k = k
+        self.b = b
+        self.remove_stopwords = remove_stopwords
+        self.apply_stemming = apply_stemming
+        self.tokenizer = RegexpTokenizer(r'\w+') 
+        if remove_stopwords:
+            self.stopwords = stopwords.words('english')
+        if apply_stemming:
+            self.stemmer = PorterStemmer()      
+
+    def train(self, documents):
+        # tokenizing documents
+        print("Tokenizing documents...")
+        documents = [self.tokenize(doc) for doc in tqdm(documents, total=len(documents))]
+        self.vocab = list(set([w for doc in documents for w in doc]))
+        self.word2idx = {w:i for i,w in enumerate(self.vocab)}
+        self.TFIDF, self.inverted_index, self.doc_tfidf_norms = self.create_inverted_index(documents)
+        
+    def tokenize(self, sent):
+        # Replace accented letters with regular letters
+        sent = unidecode(sent)
+        # tokenize into words, remove punctuation
+        words = self.tokenizer.tokenize(sent.lower())
+        # remove stopwords
+        if self.remove_stopwords:
+            words = [w for w in words if w not in self.stopwords]
+        # apply stemming to each word
+        if self.apply_stemming:    
+            words = [self.stemmer.stem(w) for w in words]
+        return words
+
+    def create_inverted_index(self, documents):
+        # compute term frequency and document frequencies
+        TF, term_docs = self.compute_TF_weighted(documents)
+
+        # create inverted index
+        N = len(documents)
+        TFIDF = lil_matrix((len(self.vocab), len(documents)))
+        inverted_index = defaultdict(list)
+        print(f"Computing TFIDF and creating inverted index...")
+        for w, docs in tqdm(term_docs.items(), total=len(term_docs)):
+            for d in sorted(list(docs)):
+                TFIDF[self.word2idx[w], d] = TF[self.word2idx[w], d] * math.log10(N/len(docs))
+                inverted_index[self.word2idx[w]].append(d)
+
+        # compute document TFIDF vector norms
+        print(f"Computing TFIDF vector norms...")
+        doc_tfidf_norms = np.zeros(N)
+        for d, words in tqdm(enumerate(documents), total=len(documents)):
+            for w in words:
+                doc_tfidf_norms[d] = doc_tfidf_norms[d] +  TFIDF[self.word2idx[w], d]**2
+            doc_tfidf_norms[d] = math.sqrt(doc_tfidf_norms[d])
+
+        return TFIDF, inverted_index, doc_tfidf_norms  
+          
+    # weighted TF for BM25
+    def compute_TF_weighted(self, documents):
+        term_docs = defaultdict(set)
+        doc_length = defaultdict(float)
+        Dtotal = 0
+        print(f"Computing TF...")
+        TF = lil_matrix((len(self.vocab), len(documents)))
+        for d, words in tqdm(enumerate(documents), total=len(documents)):
+            for w in words:
+                TF[self.word2idx[w], d] += 1
+                term_docs[w].add(d)
+            doc_length[d] = len(words)
+            Dtotal += len(words)
+        Davg = Dtotal / len(documents)
+
+        # compute BM25 weighted term frequencies
+        TF_weighted = lil_matrix((len(self.vocab), len(documents)))
+        for w, docs in term_docs.items():
+            for d in docs:
+                tf = TF[self.word2idx[w],d]
+                TF_weighted[self.word2idx[w],d] = (tf * (self.k + 1)) / (tf + self.k * (1 - self.b + self.b * (doc_length[d]/Davg)))
+        return TF_weighted, term_docs
+    
+    # @profile 
+    def retrieve_docs(self, query, topk=1):
+        # for retrieval, we will remove stopwords from the query
+        query_words = self.tokenize(query.lower())
+        # remove out of vocab words
+        query_words = [w for w in query_words if w in self.vocab]
+        if query_words == []:
+            return [], []
+        # get all documents which contain words from query
+        docs = list(set(doc for w in query_words for doc in self.inverted_index[self.word2idx[w]]))
+        # score all these documents
+        word_indices = np.array([self.word2idx[w] for w in query_words]).reshape(-1)
+        # Calculate scores using advanced indexing
+        scores = self.TFIDF[word_indices[:, None], docs].sum(axis=0) / self.doc_tfidf_norms[docs]
+        scores = np.squeeze(np.array(scores))
+        sorted_indices = np.argsort(scores)[::-1]
+        best_indices = sorted_indices[:topk].tolist()
+        topk_scores = scores[best_indices].tolist()
+        topk_doc_indices = [docs[idx] for idx in best_indices]
+        del scores
+        del docs
+        
+        return topk_doc_indices, topk_scores
+
+
+# @profile 
+def eval(train_claims, val_claims, passage_ids, retreiver, k=10, num_claims=100):
+    precision_tot = 0
+    recall_tot = 0
+    f1_tot = 0
+    num_computed = 0
+    for claims_dataset in (train_claims, val_claims):
+        for claim in tqdm(claims_dataset, total=len(claims_dataset)):
+            query = claim["claim_text"]
+            gold_evidence_list = claim["evidences"]
+            topk_doc_indices, best_scores = retreiver.retrieve_docs(query, topk=10)
+            topk_evidence_ids = [passage_ids[idx] for idx in topk_doc_indices]
+            # evaluation (precision, recall, F1)
+            intersection = set(topk_evidence_ids).intersection(gold_evidence_list)
+            precision = len(intersection) / len(topk_evidence_ids)
+            recall = len(intersection) / len(gold_evidence_list)
+            precision_tot += precision
+            recall_tot += recall
+            f1_tot += (2*precision*recall/(precision + recall)) if (precision + recall) > 0 else 0 
+            num_computed += 1
+            if num_computed == num_claims:
+                break
+        if num_computed == num_claims:
+                break
+
+    avg_precision = precision_tot / num_computed
+    avg_recall = recall_tot / num_computed
+    avg_f1 = f1_tot / num_computed
+
+    print(f"Average Precision: {avg_precision}, Average Recall: {avg_recall}, Average F1: {avg_f1}")
+
+    return avg_precision, avg_recall, avg_f1