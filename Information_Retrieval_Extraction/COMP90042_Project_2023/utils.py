6ca768674ae96f6dd909f19e6d8cc7efcb478750
tsultan16 <tanzids@student.unimelb.edu.au>
2024-01-23 17:22:36 +1100

    keep duplicate evidences

--- Information_Retrieval_Extraction/COMP90042_Project_2023/utils.py
+++ Information_Retrieval_Extraction/COMP90042_Project_2023/utils.py
@@ -25,20 +25,12 @@ def load_data(clean=False, clean_threshold=20):
     print(f"Number of validation instances: {len(val_data)}")
 
     if clean: 
-        # we remove duplicate values from the document_store dictionary (we arbitrarily keep the first one)
-        seen = set()
-        document_store_no_duplicates = {}
-        for key, value in document_store.items():
-            if value not in seen:
-                document_store_no_duplicates[key] = value
-                seen.add(value)
-
-        # remove all "bad" documents from the document store, except those that occur in claim gold evidence lists, we will define "bad" documents as ones that have less than 50 characters
+        # remove all "bad" documents from the document store, except those that occur in claim gold evidence lists, we will define "bad" documents as ones that have less than clean_threshold number of characters
         claim_evidence_list = [claim['evidences'] for claim in train_data.values()]
         claim_evidence_list = claim_evidence_list + [claim['evidences'] for claim in val_data.values()]
         claim_evidence_list = list(set([evidence for evidence_list in claim_evidence_list for evidence in evidence_list]))
 
-        document_store = {i: evidence_text for i, evidence_text in document_store_no_duplicates.items() if len(evidence_text) >= clean_threshold or i in claim_evidence_list}
+        document_store = {i: evidence_text for i, evidence_text in document_store.items() if (len(evidence_text) >= clean_threshold) or (i in claim_evidence_list)}
         print(f"Number of evidence passages remaining after cleaning: {len(document_store)}")
     
     return document_store, train_data, val_data    
